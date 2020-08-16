ecee7eb99d413a47b012527be5f845a32b065925
Zhaoyun Ma <mazhaoyunyh@gmail.com>
2020-08-16 21:17:06 +0000

    modified process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,73 @@
 import sys
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    
+    """
+    Load data function
+    
+    Input:
+        messages_filepath -> file path to the CSV file containing messages
+        categories_filepath -> Path to the CSV file containing categories
+    Output:
+        df -> Combined data containing messages and categories
+    """
+    
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages,categories,on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    
+    """
+    Clean category data function
+    
+    Input:
+        df -> Combined data frame containing messages and categories 
+    Outputs:
+        df -> Combined data frame containing messages and cleaned categories 
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = pd.DataFrame(df['categories'].str.split(';',expand=True))
+    row = categories.iloc[0]
+    category_colnames = [col.split('-')[0] for col in row]
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] =  categories[column].str.slice(start=-1)    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype('int')
+    
+    df.drop(['categories'], axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+    df.drop_duplicates(inplace=True)
+    
+    # drop the rows with all category response nan
+    df.dropna(axis=0,how='all', subset= category_colnames, inplace=True)
+#     df.drop(['child_alone'], axis=1, inplace=True)
+    # set the invalid response 2 in the 'related' category to 0
+    df.loc[df.index[df['related']==2], 'related']=0 
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    
+    """
+    Save data function (to SQLite Database) 
+    
+    Arguments:
+        df -> Combined data containing messages and cleaned categories 
+        database_filename: database name
+    """
+    engine = create_engine('sqlite:///'+ database_filename)
+    df.to_sql('messages', engine, index=False) 
 
 
 def main():
