24bf5ca51c5e23cd272e30f1c4db46111c62cef8
Zhaoyun Ma <mazhaoyunyh@gmail.com>
2020-08-16 21:35:36 +0000

    modified proces_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -80,7 +80,7 @@ def save_data(df, database_filename):
         database_filename: database name
     """
     engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('messages', engine, index=False) 
+    df.to_sql('messages', engine, index=False, if_exists='replace') 
 
 
 def main():
