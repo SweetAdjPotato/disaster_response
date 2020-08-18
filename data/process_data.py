5a02c36c0b842ace3f86532bd1a92251709d3f02
Zhaoyun Ma <mazhaoyunyh@gmail.com>
2020-08-18 11:03:14 -0400

    modified process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,6 +1,6 @@
 """
-Data Preprocessing 
-Project: Disaster Response Pipeline 
+Data Preprocessing
+Project: Disaster Response Pipeline
 Sample Script Syntax:
 > python process_data.py <path to messages csv file> <path to categories csv file> <path to sqllite  destination db>
 Sample Script Execution:
@@ -18,17 +18,17 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    
+
     """
     Load data function
-    
+
     Input:
         messages_filepath -> file path to the CSV file containing messages
         categories_filepath -> Path to the CSV file containing categories
     Output:
         df -> Combined data containing messages and categories
     """
-    
+
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = pd.merge(messages,categories,on='id')
@@ -36,51 +36,52 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
-    
+
     """
     Clean category data function
-    
+
     Input:
-        df -> Combined data frame containing messages and categories 
+        df -> Combined data frame containing messages and categories
     Outputs:
-        df -> Combined data frame containing messages and cleaned categories 
+        df -> Combined data frame containing messages and cleaned categories
     """
     # create a dataframe of the 36 individual category columns
     categories = pd.DataFrame(df['categories'].str.split(';',expand=True))
     row = categories.iloc[0]
     category_colnames = [col.split('-')[0] for col in row]
     categories.columns = category_colnames
-    
+
     for column in categories:
         # set each value to be the last character of the string
-        categories[column] =  categories[column].str.slice(start=-1)    
+        categories[column] =  categories[column].str.slice(start=-1)
         # convert column from string to numeric
         categories[column] = categories[column].astype('int')
-    
+
     df.drop(['categories'], axis=1, inplace=True)
     df = pd.concat([df, categories], axis=1)
     df.drop_duplicates(inplace=True)
-    
+
     # drop the rows with all category response nan
     df.dropna(axis=0,how='all', subset= category_colnames, inplace=True)
-#     df.drop(['child_alone'], axis=1, inplace=True)
+    # drop the child_alone column with all category response 0
+    df.drop(['child_alone'], axis=1, inplace=True)
     # set the invalid response 2 in the 'related' category to 0
-    df.loc[df.index[df['related']==2], 'related']=0 
-    
+    df.loc[df.index[df['related']==2], 'related']=0
+
     return df
 
 
 def save_data(df, database_filename):
-    
+
     """
-    Save data function (to SQLite Database) 
-    
+    Save data function (to SQLite Database)
+
     Arguments:
-        df -> Combined data containing messages and cleaned categories 
+        df -> Combined data containing messages and cleaned categories
         database_filename: database name
     """
     engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('messages', engine, index=False, if_exists='replace') 
+    df.to_sql('messages', engine, index=False, if_exists='replace')
 
 
 def main():
@@ -94,12 +95,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
